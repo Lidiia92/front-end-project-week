4f761fcb37371f6817f80dea10d01bf2e478b541
Lidii <lidiiany92@gmail.com>
2019-02-14 10:08:09 -0500

    Updates

---
+++ precache-manifest.5a7d29475761d0a12c1faa094b654de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c13ef4359f0b536b667",
+    "url": "/front-end-project-week/static/js/runtime~main.9c13ef43.js"
+  },
+  {
+    "revision": "bc3085b46945e25da2d7",
+    "url": "/front-end-project-week/static/js/main.bc3085b4.chunk.js"
+  },
+  {
+    "revision": "68bcda1e04e499c32de1",
+    "url": "/front-end-project-week/static/js/1.68bcda1e.chunk.js"
+  },
+  {
+    "revision": "bc3085b46945e25da2d7",
+    "url": "/front-end-project-week/static/css/main.7f902b45.chunk.css"
+  },
+  {
+    "revision": "84e849375fdd90c802db9a2cde64c4db",
+    "url": "/front-end-project-week/index.html"
+  }
+];
